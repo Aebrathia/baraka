805a25f0d532521a20a9ab847b0238d73b58fd4b
Andrea Maugars <andrea.maugars@gmail.com>
2020-01-05 14:23:43 +0100

    Remove CRA

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,4 +2,4 @@ import { configure } from '@storybook/react';
 import '../src/index.css';
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/components', true, /\.stories\.js$/), module);
+configure(require.context('../src', true, /\.stories\.js$/), module);
